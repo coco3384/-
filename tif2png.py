5a54584663ceac8c5a8cdd59fc05c77832db11a9
coco3384 <r10521816@ntu.edu.tw>
2023-02-22 09:21:49 +0800

    new version of tif2png.py

--- tif2png.py
+++ tif2png.py
@@ -6,34 +6,43 @@ import glob
 import argparse
 from tqdm import tqdm
 
-def min_max(array, lower_bound=0.02, upper_bound=0.98):
-    array_sort = np.sort(array)
-    min = int(array.size * lower_bound)
-    max = int(array.size * upper_bound)
-    return array_sort[min], array_sort[max]
+def save_name(fp, save_path):
+    basename = os.path.basename(fp).split('.')[0] + '.png'
+    sp = os.path.join(save_path, basename)
+    return sp
+
+def produce_png(fp, cumulative_count_cut):
+    img = rasterio.open(fp)
+    png = np.zeros((*img.shape, 0))
+    for ch in range(1, 4):
+        band = img.read(ch)
+        if cumulative_count_cut:
+            max = int(np.nanpercentile(band, 98))
+            band[band > max] = max
+        result = cv2.normalize(band, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        png = np.dstack((png, result))
+    return png.astype('float32')
+
+def not_a_rgb_img(cumulative_count_cut, sp):
+    return cumulative_count_cut and os.path.basename(sp).find('rgb') == -1
+
+def tif2png(file_path, save_path, cumulative_count_cut):
+    for fp in tqdm(file_path):
+        sp = save_name(fp, save_path)
+        cumulative_count_cut = not_a_rgb_img(cumulative_count_cut, sp)
+        png = produce_png(fp, cumulative_count_cut)
+        cv2.imwrite(sp, cv2.cvtColor(png, cv2.COLOR_RGB2BGR))
+
 
 def main(file_path, save_path,  flag='dmc', cumulative_count_cut=False):
     if cumulative_count_cut:
         save_path = save_path + '_' + flag + '_cum_cut'
+        print('will process cumulative count cut to tif image, expect there is rgb inside file name')
     else:
         save_path = save_path + '_' + flag 
     os.makedirs(save_path, exist_ok=True)
     file_path = glob.glob(os.path.join(file_path, '*.tif'))
-    for fp in tqdm(file_path):
-        basename = os.path.basename(fp).split('.')[0] + '.png'
-        sp = os.path.join(save_path, basename)
-        img = rasterio.open(fp)
-        png = np.zeros((*img.shape, 0))
-        for ch in range(1, 4):
-            band = img.read(ch)
-            band_flatten = band.flatten()
-            if cumulative_count_cut and basename.find('rgb') == -1:
-                max = int(np.nanpercentile(band_flatten, 98))
-                band[band > max] = max
-            result = cv2.normalize(band, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-            png = np.dstack((png, result))
-        png = png.astype('float32')
-        cv2.imwrite(sp, cv2.cvtColor(png, cv2.COLOR_RGB2BGR))
+    tif2png(file_path, save_path, cumulative_count_cut)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
